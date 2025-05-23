64573715b1d0bfb489c4a9e24e069441af9ae052
mojitap <ta714kadvance@gmail.com>
2025-05-23 23:44:41 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 import { registerRootComponent } from 'expo';
-import App from './screens/App.js';
+import App from './App'; // ✅ screens/ は不要
 
-// registerRootComponent calls AppRegistry.registerComponent('main', () => App);
-// It also ensures that whether you load the app in Expo Go or in a native build,
-// the environment is set up appropriately
 registerRootComponent(App);
