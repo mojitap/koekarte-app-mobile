452275a6ef6b2fd957f149dded423b2b9a35b010
mojitap <ta714kadvance@gmail.com>
2025-05-23 20:43:52 +0900

    Add files via upload

---
+++ premiumUtils.js
@@ -0,0 +1,12 @@
+// utils/premiumUtils.js
+export const checkCanUsePremium = (createdAtStr, isPaid) => {
+  if (isPaid) return true;
+  if (!createdAtStr) return false;
+
+  const createdAt = new Date(createdAtStr);
+  const today = new Date();
+  const diffTime = today - createdAt;
+  const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+
+  return diffDays < 5;
+};
