76cc2aa789e8c5e50992c4a52c77f1ead8c04873
mojitap <ta714kadvance@gmail.com>
2025-05-26 13:27:14 +0900

    Update ScoreHistory.js

--- screens/ScoreHistory.js
+++ screens/ScoreHistory.js
@@ -55,7 +55,7 @@ const styles = StyleSheet.create({
   safeArea: {
     flex: 1,
     backgroundColor: '#fff',
-    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
+    paddingTop: Platform.OS === 'android' ? (StatusBar.currentHeight || 0) : 0,
   },
   container: {
     padding: 20,
