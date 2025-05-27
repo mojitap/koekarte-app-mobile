a2ddc0a5f074d8d81ec7869aac6df8d5f7e09d7a
mojitap <ta714kadvance@gmail.com>
2025-05-27 11:25:21 +0900

    Update LegalScreen.js

--- screens/LegalScreen.js
+++ screens/LegalScreen.js
@@ -87,7 +87,7 @@ export default function LegalScreen() {
         <Text style={styles.paragraph}>最終更新日：2025年5月15日</Text>
 
         <View style={{ marginTop: 30, alignItems: 'center' }}>
-          <Button title="🏠 マイページに戻る" onPress={() => navigation.navigate('Home')} />
+          <Button title="🏠 マイページに戻る" onPress={() => navigation.navigate('Main')} />
         </View>
       </ScrollView>
     </SafeAreaView>
