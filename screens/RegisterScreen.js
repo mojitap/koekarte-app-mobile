1704cc6c3aaec47359cac56f89cb827cd49ab4d9
mojitap <ta714kadvance@gmail.com>
2025-05-27 11:21:26 +0900

    Update RegisterScreen.js

--- screens/RegisterScreen.js
+++ screens/RegisterScreen.js
@@ -38,7 +38,7 @@ export default function RegisterScreen({ navigation }) {
       await saveUser(data);  // ✅ ユーザー情報をローカルに保存
 
       Alert.alert('登録成功', 'ようこそ！', [
-        { text: 'OK', onPress: () => navigation.navigate('Home') },
+        { text: 'OK', onPress: () => navigation.navigate('Main') },
       ]);
     } catch (err) {
       console.error('❌ 登録通信エラー:', err);
