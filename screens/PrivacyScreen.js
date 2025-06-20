d305f5ecc2b627e06c09b8ebab312ae7bbb362c2
mojitap <ta714kadvance@gmail.com>
2025-06-20 11:18:43 +0900

    Update PrivacyScreen.js

--- screens/PrivacyScreen.js
+++ screens/PrivacyScreen.js
@@ -157,14 +157,14 @@ const styles = StyleSheet.create({
     marginTop: 20,
   },
   linkText: {
-    fontSize: 16,
+    fontSize: 14,
     color: '#007bff',
     marginHorizontal: 2,
     minHeight: 24,
     textDecorationLine: 'underline',
   },
   separator: {
-    fontSize: 12,
+    fontSize: 16,
     color: '#666',
   },
 });
